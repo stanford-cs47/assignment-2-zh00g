2c4eac6101edf1541d1857c5c891152903def069
AbdallahAbuHashem <aabuhash@stanford.edu>
2020-11-25 15:05:32 -0800

    Initial commit

---
+++ App.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Images, Profiles } from './App/Themes';
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+
+    var haroldProfile = Profiles.harold;
+    this.state = {
+      profileImage: haroldProfile.image,
+      name: haroldProfile.name,
+      age: haroldProfile.age,
+      occupation: haroldProfile.occupation
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>This is your workspace. Have fun!</Text>
+        <Text>(Hint: Peep the App folder)</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
